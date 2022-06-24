1b7bc6e873f402a4a4624e0868df16d985ea7be2
Maxim <maxilamb@yandex.com>
2022-06-24 15:07:16 +0300

    added mapped name:ticker

---
+++ src/const/commodities.ts
@@ -0,0 +1,17 @@
+
+export const commodities = {
+  "Gold": "GC=F",
+  "Silver": "SI=F",
+  "Copper": "HG=F",
+  "Platinum": "PA=F",
+  "Crude Oil": "CL=F",
+  "Brent Crude Oil": "BZ=F",
+  "Natural Gas": "NG=F",
+  "Heating Oil": "HO=F",
+  "Coffee": "KC=F",
+  "Corn": "ZC=F",
+  "Wheat": "KE=F",
+  "Sugar": "SB=F",
+  "Cocoa": "CC=F",
+  "Cotton": "CT=F"
+}; 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { fetch } from '../src/services/fetch/fetch';
 export { Converter } from '../src/services/converter/converter';
 export { Yahoo } from '../src/api/yahoo/yahoo';
+export {commodities} from './const/commodities'
+export {cryptocurrencies} from './const/cryptocurrencies'
+export {currencies} from './const/currencies'
