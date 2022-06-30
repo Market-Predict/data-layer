0898158321facde18193ecf92b71a75d3c118752
Maxim <maxilamb@yandex.com>
2022-06-30 19:49:48 +0300

    updated indeces

---
+++ src/const/indices.ts
@@ -0,0 +1,111 @@
+
+export const indices = [
+  {
+    name: 'Dow Jones',
+    key: "^DJI",
+    country: "US",
+    region: "North America"
+  },
+  {
+    name: 'NASDAQ 100',
+    key: "^NDX",
+    country: "US",
+    region: "North America"
+  },
+  {
+    name: 'NASDAQ Composite',
+    key: "^IXIC",
+    country: "US",
+    region: "North America"
+  },
+  {
+    name: 'S&P 500',
+    key: "^GSPC",
+    country: "US",
+    region: "North America"
+  },
+  {
+    name: 'CBOE Volatility',
+    key: "^VIX",
+    country: "US",
+    region: "North America"
+  },
+  {
+    name: 'US Dollar',
+    key: "DX-Y.NYB",
+    country: "US",
+    region: "North America"
+  },
+  {
+    name: 'AEX',
+    key: "^AEX",
+    country: "NL",
+    region: "Europe"
+  },
+  {
+    name: 'CAC 40',
+    key: "^FCH",
+    country: "FR",
+    region: "Europe"
+  },
+  {
+    name: 'DAX',
+    key: "^GDAXI",
+    country: "DE",
+    region: "Europe"
+  },
+  {
+    name: 'Euro Stoxx 50',
+    key: "^STOXX50E",
+    country: "DE",
+    region: "Europe"
+  },
+  {
+    name: 'FTSE 100',
+    key: "^FTSE",
+    country: "GB",
+    region: "Europe"
+  },
+  {
+    name: 'FTSE MIB INDEX',
+    key: "FTSEMIB.MI",
+    country: "IT",
+    region: "Europe"
+  },
+  {
+    name: 'IBEX 35',
+    key: "^IBEX",
+    country: "ES",
+    region: "Europe"
+  },
+  {
+    name: 'MOEX Russia',
+    key: "IMOEX.ME",
+    country: "RU",
+    region: "Europe"
+  },
+  {
+    name: 'RTSI',
+    key: "RTSI.ME",
+    country: "RU",
+    region: "Europe"
+  },
+  {
+    name: 'FTSE China A50',
+    key: "XIN9.FGI",
+    country: "CN",
+    region: "Asia"
+  },
+  {
+    name: 'Hang Seng',
+    key: "^HSI",
+    country: "CN",
+    region: "Asia"
+  },
+  {
+    name: 'Nikkei 225',
+    key: "^N225",
+    country: "JP",
+    region: "Asia"
+  },
+]
