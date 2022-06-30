779d26c28d1ae1d936d3ef32a1573d295985c1cd
Maxim <maxilamb@yandex.com>
2022-06-30 19:51:33 +0300

    updated

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,8 +1,10 @@
+import { indices } from './indices';
 import { commodities } from './commodities';
 import { cryptocurrencies } from './cryptocurrencies';
 import { currencies } from './currencies';
 
 export const constants = {
+  indices,
   commodities,
   cryptocurrencies,
   currencies,
