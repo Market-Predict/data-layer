8c0fd488a22808189daa678056a9f22e8c1fe673
Maxim <maxilamb@yandex.com>
2022-07-02 16:05:27 +0300

    added commodities

---
+++ src/api/stooq/const /commodities.ts
@@ -0,0 +1,31 @@
+export const commodities = {
+  energy: {
+    'CRUDE OIL BRENT': 'CB.F',
+    'CRUDE OIL WTI': 'CL.F',
+    'ULSD NY HARBOR': 'HO.F',
+    'GASOIL LOW SULPHUR': 'LF.F',
+    'NATURAL GAS': 'NG.F',
+  },
+  metals: {
+    GOLD: 'aaa',
+    'HIGH GRADE COPPER': 'HG.F',
+    PALLADIUM: 'PA.F',
+    PLATINUM: 'PL.F',
+    SILVER: 'SI.F',
+  },
+  grains: {
+    CORN: 'ZC.F',
+    'SOYBEAN OIL': 'ZL.F',
+    SOYBEAN: 'ZS.F',
+    WHEAT: 'ZW.F',
+  },
+  softs: {
+    COCOA: 'CC.F',
+    COFFEE: 'KC.F',
+    'ORANGE JUICE': 'OJ.F',
+    SUGAR: 'SB.F',
+  },
+  fibers: {
+    COTTON: 'CT.F',
+  },
+};
