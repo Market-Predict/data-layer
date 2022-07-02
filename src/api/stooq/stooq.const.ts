c2b65df0287d9a9db5b5df5434303dc44e949502
Maxim <maxilamb@yandex.com>
2022-07-02 15:15:51 +0300

    added indices

---
+++ src/api/stooq/stooq.const.ts
@@ -0,0 +1,305 @@
+export const indices = {
+  asia: [
+    {
+      key: '^AOR',
+      name: 'ALL ORDINARIES',
+      country: 'AU',
+    },
+    {
+      key: '^HSI',
+      name: 'HANG SENG INDEX',
+      country: 'CN',
+    },
+    {
+      key: '^JCI',
+      name: 'JAKARTA COMPOSITE',
+      country: 'ID',
+    },
+    {
+      key: '^KLCI',
+      name: 'KUALA LUMPUR COMPOSITE',
+      country: 'MY',
+    },
+    {
+      key: '^KOSPI',
+      name: 'KOSPI',
+      country: 'KR',
+    },
+    {
+      key: '^NKX',
+      name: 'NIKKEI 225',
+      country: 'JP',
+    },
+    {
+      key: '^NZ50',
+      name: 'NZX 50',
+      country: 'NZ',
+    },
+    {
+      key: '^PSEI',
+      name: 'PSEI',
+      country: 'PH',
+    },
+    {
+      key: '^SET',
+      name: 'SET',
+      country: 'TH',
+    },
+    {
+      key: '^SHBS',
+      name: 'SHANGHAI B-SHARE',
+      country: 'CN',
+    },
+    {
+      key: '^SHC',
+      name: 'SHANGHAI COMPOSITE',
+      country: 'CN',
+    },
+    {
+      key: '^SNX',
+      name: 'SENSEX 30',
+      country: 'IN',
+    },
+    {
+      key: '^STI',
+      name: 'STRAITS TIMES',
+      country: 'SG',
+    },
+    {
+      key: '^TWSE',
+      name: 'TAIEX',
+      country: 'TW',
+    },
+  ],
+  europe: [
+    {
+      key: 'A5.C',
+      name: 'ATX',
+      country: 'AT',
+    },
+    {
+      key: '^AEX',
+      name: 'AEX',
+      country: 'NL',
+    },
+    {
+      key: '^ATH',
+      name: 'ATHEX COMPOSITE',
+      country: 'GR',
+    },
+    {
+      key: '^BEL20',
+      name: 'BEL 20',
+      country: 'BE',
+    },
+    {
+      key: '^BET',
+      name: 'BET',
+      country: 'RO',
+    },
+    {
+      key: '^BUX',
+      name: 'BUX',
+      country: 'HU',
+    },
+    {
+      key: '^CAC',
+      name: 'CAC 40',
+      country: 'FR',
+    },
+    {
+      key: '^DAX',
+      name: 'DAX',
+      country: 'DE',
+    },
+    {
+      key: '^FMIB',
+      name: 'FTSE MIB',
+      country: 'IT',
+    },
+    {
+      key: '^FTM',
+      name: 'FTSE 250 ',
+      country: 'UK',
+    },
+    {
+      key: '^HEX',
+      name: 'OMX HELSINKI',
+      country: 'FI',
+    },
+    {
+      key: '^IBEX',
+      name: 'IBEX',
+      country: 'ES',
+    },
+    {
+      key: '^ICEX',
+      name: 'OMX ICELAND ALL SHARE',
+      country: 'IS',
+    },
+    {
+      key: '^MDAX',
+      name: 'MDAX',
+      country: 'DE',
+    },
+    {
+      key: '^MOEX',
+      name: 'MOEX',
+      country: 'RU',
+    },
+    {
+      key: '^OMXR',
+      name: 'OMX RIGA',
+      country: 'LV',
+    },
+    {
+      key: '^OMXS',
+      name: 'OMX STOCKHOLM 30',
+      country: 'SE',
+    },
+    {
+      key: '^OMXT',
+      name: 'OMX TALLINN',
+      country: 'EE',
+    },
+    {
+      key: '^OMXV',
+      name: 'OMX VILNIUS',
+      country: 'LT',
+    },
+    {
+      key: '^OSEAX',
+      name: 'OSE ALL SHARE',
+      country: 'NO',
+    },
+    {
+      key: '^PSI20',
+      name: 'PSI 20',
+      country: 'PT',
+    },
+    {
+      key: '^PX',
+      name: 'PX',
+      country: 'CZ',
+    },
+    {
+      key: '^RTS',
+      name: 'RTS',
+      country: 'RU',
+    },
+    {
+      key: '^SAX',
+      name: 'SAX',
+      country: 'SK',
+    },
+    {
+      key: '^SDXP',
+      name: 'SDAX',
+      country: 'DE',
+    },
+    {
+      key: '^SMI',
+      name: 'SWISS MARKET',
+      country: 'CH',
+    },
+    {
+      key: '^SOFIX',
+      name: 'SOFIX',
+      country: 'BG',
+    },
+    {
+      key: '^TDXP',
+      name: 'TECDAX',
+      country: 'DE',
+    },
+    {
+      key: '^UKX',
+      name: 'UK 100 CFD',
+      country: 'UK',
+    },
+    {
+      key: '^UX',
+      name: 'UX',
+      country: 'UA',
+    },
+    {
+      key: 'WIG',
+      name: 'WIG',
+      country: 'PL',
+    },
+    {
+      key: 'WIG20',
+      name: 'WIG20',
+      country: 'PL',
+    },
+    {
+      key: '^XU100',
+      name: 'XU100',
+      country: 'TR',
+    },
+  ],
+  america: [
+    {
+      key: '^DJC',
+      name: 'DOW JONES COMPOSITE',
+      country: 'US',
+    },
+    {
+      key: '^DJI',
+      name: 'DOW JONES INDUSTRIAL',
+      country: 'US',
+    },
+    {
+      key: '^DJT',
+      name: 'DOW JONES TRANSPORTATION',
+      country: 'US',
+    },
+    {
+      key: '^DJU',
+      name: 'DOW JONES UTILITIES',
+      country: 'US',
+    },
+    {
+      key: '^NDQ',
+      name: 'NASDAQ COMPOSITE',
+      country: 'US',
+    },
+    {
+      key: '^NDX',
+      name: 'NASDAQ 100',
+      country: 'US',
+    },
+    {
+      key: '^SPX',
+      name: 'S&P 500',
+      country: 'US',
+    },
+    {
+      key: '^BVP',
+      name: 'BOVESPA',
+      country: 'BR',
+    },
+    {
+      key: '^IPC',
+      name: 'MEXICAN BOLSA',
+      country: 'MX',
+    },
+    {
+      key: '^IPSA',
+      name: 'IPSA',
+      country: 'CL',
+    },
+    {
+      key: '^MRV',
+      name: 'MERVAL',
+      country: 'AR',
+    },
+  ],
+  middleEast: [
+    {
+      key: '^TASI',
+      name: 'TADAWUL ALL SHARE',
+      country: 'SA',
+    },
+  ],
+};
