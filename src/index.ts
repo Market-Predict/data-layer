dc767f2b1671ce94b8f84f0577e13ef8644ee531
Maxim <maxilamb@yandex.com>
2022-07-03 04:31:01 +0300

    fix issue

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { fetch } from '../src/services/fetch/fetch';
 export { Converter } from '../src/services/converter/converter';
 export { Yahoo } from '../src/api/yahoo/yahoo';
+export { Stooq } from '../src/api/stooq/stooq';
 export { ForexClub } from '../src/api/forexclub/forexclub';
